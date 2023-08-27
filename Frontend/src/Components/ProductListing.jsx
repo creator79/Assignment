2b771286fec58b1e6f71dba08a87e438d56ecf34
mahi-19 <upadhyayvivek.19@gmail.com>
2023-08-27 12:52:42 +0530

    feat: update Apis and fixed style issues

--- Frontend/src/Components/ProductListing.jsx
+++ Frontend/src/Components/ProductListing.jsx
@@ -1,24 +1,26 @@
-/* eslint-disable*/
-import React, { useState, useEffect } from "react";
+/*eslint-disable*/
+import React, { useState, useEffect, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import "./Css/ProductListing.css";
 import SinglePage from "./Products";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 function Products() {
   const [data, setData] = useState([]);
   const [filterData, setFilterData] = useState([]);
   const [filters, setFilters] = useState(false);
   const [error, setError] = useState(false); // State to track the error
+  const sliderRef = useRef(null); // Reference to the Slider component
 
   useEffect(() => {
     handleGetData();
   }, []);
 
   const handleGetData = () => {
-    fetch("https://api.escuelajs.co/api/v1/products")
+    fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
       .then((jsonData) => {
         console.log(jsonData);
@@ -48,7 +50,7 @@ function Products() {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 2,
+          slidesToScroll: 1, // Change this to 1 for better navigation on small screens
         },
       },
     ],
@@ -57,46 +59,62 @@ function Products() {
   const handleFilter = (name) => {
     setFilters(true);
     let newData = data.filter((e) => {
-      if (e.category.name === name) {
+      if (e.category === name) {
         return e;
       }
       if (!name) {
         return e;
       }
-      return;
+      return null; // Return null instead of undefined
     });
     setFilterData(newData);
+    sliderRef.current.slickGoTo(0); // Reset slider position when changing filters
   };
-
   return (
     <div className="productpage">
       <div className="newproducts">New products</div>
       <div className="underline"></div>
       <div className="displaydata">
         <div className="category">
-          <p onClick={() => handleFilter("Apparel")}>Apparel</p>
-          <p onClick={() => handleFilter("Accessories")}>Accessories</p>
-          <p onClick={() => handleFilter("Best sellers")}>Best sellers</p>
-          <p onClick={() => handleFilter(" 50% off")}>50% off</p>
+        <p onClick={() => handleFilter("electronics")}>Apparel</p>
+          <p onClick={() => handleFilter("jewelery")}>Accessories</p>
+          <p onClick={() => handleFilter("men's clothing")}>Best sellers</p>
+          <p onClick={() => handleFilter("women's clothing")}>50% off</p>
         </div>
-        <div className="apidata">
-          <div>
-            {error ? (
-              <p className="error-message">Api Limit is Crossed.</p>
-            ) : (
-              <Slider {...settings}>
-                {!filters
-                  ? data.map((product) => {
-                      return <SinglePage {...product} />;
-                    })
-                  : filterData.map((e) => <SinglePage {...e} />)}
-              </Slider>
-            )}
+        <div className="carousel-navigation">
+            <button
+              className=" prev-button"
+              onClick={() => sliderRef.current.slickPrev()}
+            >
+              <FaChevronLeft />
+            </button>
+           
           </div>
+        <div className="apidata">
+      
+          {error ? (
+            <p className="error-message">Api Limit is Crossed.</p>
+          ) : (
+            <Slider ref={sliderRef} {...settings}>
+              {!filters
+                ? data.map((product) => {
+                    return <SinglePage key={product.id} {...product} />;
+                  })
+                : filterData.map((e) => <SinglePage key={e.id} {...e} />)}
+            </Slider>
+          )}
+         
+        </div>
+        <div className="carousel-navigation">
+        <button
+              className=" next-button"
+              onClick={() => sliderRef.current.slickNext()}
+            >
+              <FaChevronRight />
+            </button>
         </div>
       </div>
     </div>
   );
 }
-
 export default Products;
--- Frontend/src/Components/Products.jsx
+++ Frontend/src/Components/Products.jsx
@@ -2,13 +2,15 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-// import "./Css/Products.css";
+import "./Css/ProductListing.css";
+import arrow from "../assets/images/arrow.png";
 
 const SinglePage = (product) => {
   return (
     <div key={product.id}>
       <div className="display-data">
-        <img className="api-image" src={product.images[0]} />
+        <img className="api-image" src={product.image} />
+      <img className="arrow" src={arrow} alt="arrow" />
         <p className="pd-name">{product.category.title}</p>
         <p className="pd-des">{product.description}</p>
         <p className="pd-price">${product.price}</p>
